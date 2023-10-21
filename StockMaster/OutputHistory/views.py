69249ff9d56ed58cb86eb9e83ae22b8b95b98333
Santiago <48573143+RovleZMex@users.noreply.github.com>
2023-10-21 12:30:48 -0700

    Prove that we can implement two models to track the orders by worker and know what they took out from the storage

---
+++ StockMaster/OutputHistory/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+
+from .models import Worker, OutputOrder
+
+'''
+I wanted to prove that we can link orders with multiple products to a singular worker
+With the use of a new model OutputOrder that stores the worker object, and a helper model
+OutputOrderItem, this model stores a singular product and the quantity that was taken in this order,
+and is linked to a OutputOrder. We can pin point a worker searching by name, or worker code with:
+
+ >> worker = Worker.objects.get(name='Karl')
+or 
+ >> worker = Worker.objects.get(workerCode=935431)
+
+We can then access all of the orders attached to a worker with:
+
+ >> allOrders = OutputOrder.objects.filter(worker=worker)
+
+allOrders will contain an array of OutputOrder models with all the orders attached to worker, to then access the items 
+and quantities for each item in the order we can do it with:
+
+ >> allItemsInOrder = allOrders[index].GetItems()
+ 
+allItemsInOrder is an array of OutputOrderItem models, each of them contains a Product model, and quantity
+for each, por example:
+
+ >> allItemsInOrder[0].product.name -> "Esponja"
+ >> allItemsInOrder[0].quantity -> 2
+
+What that means is that Karl took 2 esponjas from the storage.
+
+To create a new order we first have to create a Worker model, create a new OutputOrder and link it to the worker. 
+Then we can create OutputOrderItem and link it to the OutputOrder just created.
+
+
+| Worker 
+    | OutputOrder  (A Worker can have multiple OutputOrders but the OutputOrder only one Worker)
+        | OutputOrderItem (A OutputOrder can have multiple OutputOrderItems but the OutputOrderItems only one OutputOrder)
+                          (A Product can have multiple OutputOrderItems but the OutputOrderItem only one Product)
+            | Product 
+'''
+
+
+# Create your views here.
+def outputHistory(request):
+    worker = Worker.objects.get(name="Juan")
+    orders = OutputOrder.objects.filter(worker=worker)
+    context = {
+        'orders': orders
+    }
+
+    return render(request, 'outputHistory.html', context)

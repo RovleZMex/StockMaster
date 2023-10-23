6cddcd1d5ddc11cf3b74e5460d45968c4aaca4fe
MasterHDXD <masterhdxd@gmail.com>
2023-10-22 21:28:28 -0700

    HistoryExample
    Modified frontend of History-Approach-Santy by Santy

--- StockMaster/Product/views.py
+++ StockMaster/Product/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Worker, OutputOrder
 
 # Create your views here.
+def history(request):
+    worker = Worker.objects.get(name="Juan")
+    orders = OutputOrder.objects.filter(worker=worker)
+    context = {
+        'orders': orders
+    }
+
+    return render(request, 'EntryHistory.html', context)

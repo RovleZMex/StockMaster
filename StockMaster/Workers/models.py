d180f2615a2fdd153e180b09e6f37f61133dcc9b
Dreamchuyito03 <a221201170@unison.mx>
2023-10-23 17:28:53 -0700

    Added new format
    Completed task

--- StockMaster/Workers/models.py
+++ StockMaster/Workers/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-name = models.CharField(max_length=255)  # Product name
+name = models.CharField(max_length=255)  # Employee name
 employeeNumber = models.IntegerField(max_length=255)  # Employee number
 workArea = models.CharField(max_length=255)  # Work area of employee
 

e9a0dd4573f8e14c24e00b89c09fd471c4e40a73
Manumoys <misaacmdz@gmail.com>
2023-10-23 21:21:05 -0700

    Inventory page
    Updated inventory page, added limit to how many products can be shown on screen, also changed the visuals on the stock dropdown

--- StockMaster/Inventory/urls.py
+++ StockMaster/Inventory/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.inventory, name="inventory")
+    path('', views.inventory, name="inventory"),
 ]

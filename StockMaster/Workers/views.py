052e0cced5786fcdab52823016e5f83b61df5113
Dreamchuyito03 <a221201170@unison.mx>
2023-10-26 12:50:50 -0700

    Advancement in removing the hardcoded items

--- StockMaster/Workers/views.py
+++ StockMaster/Workers/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from Product.models import Product
 
+
 # Create your views here.
 def store(request):
-    products = Product.objects.all()
-    print("Products:", products)
-    context = {'products': products}
-    return render(request,'store/store.html')
+    products = Product.objects.all()  # REGRESA LAS INSTANCIAS DE TODOS LOS PRODUCTOS EN LA BASE DE DATOS
+    context = {
+        "products": products,
+    }
+    return render(request, 'store/store.html', context)

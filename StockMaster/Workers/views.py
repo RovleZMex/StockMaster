2e0b408969445cafc0163b794282dd80a70e6a5d
Dreamchuyito03 <a221201170@unison.mx>
2023-10-26 15:10:38 -0700

    New look on "store"

--- StockMaster/Workers/views.py
+++ StockMaster/Workers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from Product.models import Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -8,4 +9,5 @@ def store(request):
     context = {
         "products": products,
     }
+    product_count = products.count()
     return render(request, 'store/store.html', context)

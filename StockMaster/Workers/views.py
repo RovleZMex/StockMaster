b13ccf24786f247c722c9916abc960cdbd3256f0
Jesus Antonio <124739807+Dreamchuyito03@users.noreply.github.com>
2023-10-25 22:53:29 -0700

    Test for the workers view
    Need to fix bugs

--- StockMaster/Workers/views.py
+++ StockMaster/Workers/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from Product.models import Product
 
 # Create your views here.
 def store(request):
+    products = Product.objects.all()
+    print("Products:", products)
+    context = {'products': products}
     return render(request,'store/store.html')

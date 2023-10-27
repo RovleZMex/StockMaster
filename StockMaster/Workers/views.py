1858ea64c5b71c3602948a155e5bb7a4fb10fa08
Dreamchuyito03 <a221201170@unison.mx>
2023-10-27 13:45:36 -0700

    Added search bar

--- StockMaster/Workers/views.py
+++ StockMaster/Workers/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from Product.models import Product
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 
-
-# Create your views here.
 def store(request):
-    products = Product.objects.all()  # REGRESA LAS INSTANCIAS DE TODOS LOS PRODUCTOS EN LA BASE DE DATOS
+    query = request.GET.get('query', '')
+    product_list = Product.objects.filter(Q(name__icontains=query))
+    paginator = Paginator(product_list, 9)  # Show 10 products per page
+
+    page = request.GET.get('page') or 1
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        products = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        products = paginator.page(paginator.num_pages)
+
     context = {
         "products": products,
+        "product_count": products.paginator.count,
+        "query": query,
     }
-    product_count = products.count()
     return render(request, 'store/store.html', context)

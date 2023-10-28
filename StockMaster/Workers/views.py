a58b36b81ea29ef3a57af8343fa2e49155ff5c88
Jesus Antonio <124739807+Dreamchuyito03@users.noreply.github.com>
2023-10-27 19:16:02 -0700

    fixed search bar added category filter and cart function

--- StockMaster/Workers/views.py
+++ StockMaster/Workers/views.py
@@ -1,26 +1,42 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render,redirect
 from Product.models import Product
 from django.db.models import Q
 
+
 def store(request):
     query = request.GET.get('query', '')
-    product_list = Product.objects.filter(Q(name__icontains=query))
-    paginator = Paginator(product_list, 9)  # Show 10 products per page
+    category = request.GET.get('category', '')
 
-    page = request.GET.get('page') or 1
-    try:
-        products = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        products = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        products = paginator.page(paginator.num_pages)
+    products = Product.objects.filter(Q(name__icontains=query))
+
+    if category:
+        products = products.filter(category=category)
 
     context = {
         "products": products,
-        "product_count": products.paginator.count,
+        "product_count": products.count(),
         "query": query,
+        "categories": Product._meta.get_field('category').choices,  # Get category choices directly from the model field
+        "selected_category": category,
     }
+
+    # Handle adding to cart
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+        product = Product.objects.get(id=product_id)
+        cart = request.session.get('cart', {})
+        cart[product_id] = cart.get(product_id, 0) + 1
+        request.session['cart'] = cart
+        return redirect('store')
+
+    # Get cart from session
+    cart = request.session.get('cart', {})
+    cart_items = []
+    for product_id, quantity in cart.items():
+        product = Product.objects.get(id=product_id)
+        cart_items.append({'product': product, 'quantity': quantity})
+
+    context.update({
+        'cart_items': cart_items,
+    })
     return render(request, 'store/store.html', context)

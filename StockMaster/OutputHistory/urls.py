61298cd797078b22d795b1d23c34becfd0bc63d2
MasterHDXD <masterhdxd@gmail.com>
2023-11-09 12:06:26 -0700

    Added clickable rows

--- StockMaster/OutputHistory/urls.py
+++ StockMaster/OutputHistory/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('salidas/', views.OutputHistory, name="outputHistory"),  # Different path for the outputHistory view
     path('entradas/', views.InputHistory, name="inputHistory"),  # Different path for the inputHistory view
     path('detallesTrabajador/<int:orderid>/', views.outputDetails, name="outputDetails"),  # path to go to order details
-    path('inventario/', views.redirection, name="inventory"),  # path for the redirection view
 ]

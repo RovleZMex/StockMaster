d6cdf7aeed48419ab6a863674e1b40f430633b1f
Santiago <48573143+RovleZMex@users.noreply.github.com>
2023-11-15 08:40:38 -0700

    ADDED EXPENSES GRAPHS, OTHER SMALL FIXES.

--- StockMaster/Charts/views.py
+++ StockMaster/Charts/views.py
@@ -13,6 +13,7 @@ from django.template.loader import get_template
 from django.views import View
 from xhtml2pdf import pisa
 
+from InputHistory.models import InputOrder
 from Product.models import Product
 
 
@@ -73,6 +74,14 @@ class ViewPDF(View):
             return HttpResponse(pdf, content_type='application/pdf')
 
 
+@login_required(login_url="login")
+def ExpensesCharts(request):
+    context = {
+        'years': range(2023, datetime.now().year + 1)
+    }
+    return render(request, 'expenses-graph.html', context)
+
+
 @login_required(login_url='login')
 def ReportCharts(request):
     categories = ['Eléctrico', 'Plomería', 'Oficina', 'Limpieza']
@@ -115,10 +124,14 @@ def GetStockMonth(request):
             for product in Product.objects.all():
                 product = product
                 time = date(year, month, i)
+
                 try:
-                    dayTotal += product.history.as_of(time).quantity
+                    if datetime.now().day == i and datetime.now().month == month and datetime.now().year == year:
+                        dayTotal += product.quantity
+                    else:
+                        dayTotal += product.history.as_of(time).quantity
                 except ObjectDoesNotExist:
-                    continue
+                    pass
             stock.append(dayTotal)
         return JsonResponse({
             'success': True,
@@ -147,6 +160,41 @@ def GetCategoriesMonth(request):
     })
 
 
+@login_required(login_url='login')
+def GetExpensesPerCategoryMonth(request):
+    if request.method == "POST":
+        year = int(request.POST.get("year"))
+        month = int(request.POST.get("month"))
+        orders = getOrdersInMonthAndYear(month, year)
+        quantities = getExpensesPerCategory(orders)
+        return JsonResponse({
+            'success': True,
+            'data': json.dumps(quantities),
+        })
+    return JsonResponse({
+        'success': False,
+    })
+
+
+@login_required(login_url='login')
+def GetExpensesPercentages(request):
+    if request.method == "POST":
+        print("POOOSSTINNGGG")
+        year = int(request.POST.get("year"))
+        month = int(request.POST.get("month"))
+        orders = getOrdersInMonthAndYear(month, year)
+        percentages = getExpensesPercentagesPerCategory(orders)
+        print(f"Porcentajes: {percentages}")
+        return JsonResponse({
+            'success': True,
+            'data': json.dumps(percentages)
+        })
+    else:
+        return JsonResponse({
+            'success': False,
+        })
+
+
 @login_required(login_url="login")
 def GetPercentagesMonth(request):
     if request.method == "POST":
@@ -166,11 +214,49 @@ def GetPercentagesMonth(request):
 
 
 @login_required(login_url='login')
+def GetExpensesMonth(request):
+    if request.method == "POST":
+        year = int(request.POST.get("year"))
+        month = int(request.POST.get("month"))
+        if datetime.now().month == month:
+            daysRange = range(1, datetime.now().day + 1)
+        elif month > datetime.now().month or year > datetime.now().year:
+            daysRange = range(0)
+        else:
+            daysRange = range(1, calendar.monthrange(year, month)[1] + 1)
+        orders = getOrdersInMonthAndYear(month, year).order_by("date_created")
+        expenses = []
+        labels = []
+        expIndex = 0
+        for i in daysRange:
+            tempDate = date(year, month, i)
+            labels.append(f"Día {i}")
+            if expIndex < len(orders) and orders[expIndex].date_created.date() == tempDate:
+                try:
+                    expenses.append(expenses[i - 2] + orders[expIndex].GetTotal())
+                    expIndex += 1
+                except IndexError:
+                    expenses.append(orders[expIndex].GetTotal())
+                    expIndex += 1
+            elif len(expenses) == 0:
+                expenses.append(0)
+            else:
+                expenses.append(expenses[-1])
+        return JsonResponse({
+            'success': True,
+            'data': json.dumps(expenses),
+            'labels': ','.join(labels)
+        })
+    return JsonResponse({
+        'success': False,
+    })
+
+
+@login_required(login_url='login')
 def TextInventory(request):
     years = range(2023, datetime.now().year + 1)
     month = None
     if request.method == "POST":
-        print("POSTIIIIIINGGGG")
         year = int(request.POST.get("year"))
         month = int(request.POST.get("month"))
         if int(request.POST.get("month")) != datetime.now().month or int(
@@ -218,6 +304,26 @@ def TextInventory(request):
     return render(request, 'report-invText.html', context)
 
 
+def getExpensesPerCategory(orders):
+    quantities = [0, 0, 0, 0]
+    for order in orders:
+        for productItem in order.inputorderitem_set.all():
+            if productItem.product.category == "ELE":
+                quantities[0] += productItem.getSubtotal()
+            elif productItem.product.category == "PLU":
+                quantities[1] += productItem.getSubtotal()
+            elif productItem.product.category == "OFF":
+                quantities[2] += productItem.getSubtotal()
+            elif productItem.product.category == "CLE":
+                quantities[3] += productItem.getSubtotal()
+    return quantities
+
+
+def getOrdersInMonthAndYear(month, year):
+    return InputOrder.objects.filter(date_created__month=month,
+                                     date_created__year=year)
+
+
 def GetInventoryAsOfDate(dateA):
     inventory = []
     if dateA <= datetime.now().date() or dateA.month == datetime.now().month:
@@ -227,6 +333,16 @@ def GetInventoryAsOfDate(dateA):
             except ObjectDoesNotExist:
                 pass
     return inventory
+
+
+def getExpensesPercentagesPerCategory(orders):
+    quantities = getExpensesPerCategory(orders)
+    if sum(quantities) != 0:
+        percentages = [round((x * 100) / sum(quantities), 2) for x in quantities]
+    else:
+        percentages = [0, 0, 0, 0]
+    print(f"AAAAAAAAAAAA --- {percentages}")
+    return percentages
 
 
 def GetPercentagesPerCategory(inventory):
